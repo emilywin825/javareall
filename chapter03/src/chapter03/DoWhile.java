e5d99560e0b28684f3e09073b495494986bf11d6
emilywin825 <emilywin825@naver.com>
2022-04-03 11:53:00 +0900

    commit

--- chapter03/src/chapter03/DoWhile.java
+++ chapter03/src/chapter03/DoWhile.java
@@ -5,7 +5,7 @@ public class DoWhile {
 	{
 		Scanner input=new Scanner (System.in);
 		/*
-		 * int month; do {System.out.print("올바른 월을 입력하시오 [1-11]:");
+		 * int month; do {System.out.print("올바른 월을 입력하시오 [1-12]:");
 		 * month=input.nextInt(); }while(month<1|month>=13);
 		 * System.out.println("사용자가 입력한 월은"+month);
 		 */
