62532beda1ce7ffd9079434600e8d59da5e9eff4
emilywin825 <emilywin825@naver.com>
2022-04-08 23:48:03 +0900

    commit

---
+++ chapter04/src/chapter04/Array.java
@@ -0,0 +1,18 @@
+package chapter04;
+
+import java.util.Scanner;
+
+public class Array {
+	public static void main(String args[])
+	{
+		/*
+		 * Scanner input=new Scanner(System.in); int people=5; int sum=0; int[]
+		 * score=new int[people];
+		 * 
+		 * for(int i=0;i<score.length;i++) { System.out.print("성적 입력:");
+		 * score[i]=input.nextInt(); sum+=score[i]; }
+		 * System.out.println("평균 성적은"+sum/people+"입니다.");
+		 */
+		Char[] pizza= {'Pepperoni'};
+	}
+}
