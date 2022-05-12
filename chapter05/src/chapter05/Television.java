edf34ef7106c3c1d49c2227e490a8c679b82ce2e
emilywin825 <emilywin825@naver.com>
2022-05-12 23:53:50 +0900

    commit

--- chapter05/src/chapter05/Television.java
+++ chapter05/src/chapter05/Television.java
@@ -1,8 +1,16 @@
 package chapter05;
 
 public class Television {
+	int channel;
+	int volume;
+	boolean onOff;
+	
 	public static void main(String args[])
 	{
-		
+		Television tv=new Television();
+		tv.channel=7;
+		tv.volume=0;
+		tv.onOff=true;
+		System.out.println("텔레비전의 채널은"+tv.channel+"이고 볼륨은"+tv.volume+"입니다.");
 	}
 }
