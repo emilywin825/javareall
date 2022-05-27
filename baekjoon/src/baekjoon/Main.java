8cb6b9c57471d28f181b2b81d93d05075049f224
emilywin825 <emilywin825@naver.com>
2022-05-27 23:55:14 +0900

    commit

--- baekjoon/src/baekjoon/Main.java
+++ baekjoon/src/baekjoon/Main.java
@@ -2,18 +2,16 @@ package baekjoon;
 
 import java.util.Scanner;
 public class Main{
-public static void main(String[] args){
-    Scanner sc=new Scanner(System.in);
-    int number=sc.nextInt();
-    int a1,a2=sc.nextInt();
-    int a3,a4=sc.nextInt();
-    int a5,a6=sc.nextInt();
-    int a7,a8=sc.nextInt();
-    int a9,a10=sc.nextInt();
-    
-    for(int i=1;i<=number;i++)
+    public static void main(String[] args)
     {
-        System.out.println("Case #"+i+":");
-    }
-    }
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int x=sc.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            int a=sc.nextInt();
+            if(a<x)
+        	{System.out.println(a+" ");}
+        }    
+     }
 }
--- chapter05/src/chapter05/Time.java
+++ chapter05/src/chapter05/Time.java
@@ -1,37 +1,7 @@
 package chapter05;
 public class Time {
-	
-	private int hour;
-	private int minute;
-	private int second;
-	
-	public Time()
+	public static void main(String[] args)
 	{
-		this(0,0,0);
-	}
-
-	public Time(int h,int m,int s)
-	{
-		hour=((h>=0&&h<24) ?h :0);
-		minute=((m>=0&&m<60)?m:0);
-		second=((s>=00&&s<60)?s:0);
-	}
-	
-	@Override
-	public String toString() {
-		return String.format("%02d:%02d:%02d:",hour,minute,second );
-	}
-
-	public static void main(String[] args) {
-		Time time1=new Time();
-		System.out.println(time1);
 		
-		Time time2=new Time(13,27,06);
-		System.out.println(time2.toString());
-		
-		Time time3=new Time(-13,27,06);
-		System.out.println(time3);
-
 	}
-
 }
